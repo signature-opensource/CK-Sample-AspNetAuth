b9371d666384dab59cc7b3d6ba989f570dd8884c
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-11-12 13:15:01 +0100

    Fixed changes.

--- UserOidc/UserOidc.Builder/Program.cs
+++ UserOidc/UserOidc.Builder/Program.cs
@@ -13,8 +13,6 @@ return await CKomposableAppBuilder.RunAsync( ( monitor, builder ) =>
 
     var angularDemo = new TypeScriptBinPathAspectConfiguration();
     tester.AddOtherConfiguration( angularDemo );
-    angularDemo.AutoInstallYarn = true;
-    angularDemo.GitIgnoreCKGenFolder = true;
     angularDemo.TargetProjectPath = builder.GetHostFolderPath().Combine( "Clients/AngularDemo" );
     angularDemo.TypeFilterName = "TypeScriptAngularDemo";
 } );
